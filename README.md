b12f8782d361cd2c861d33cff82f402ff2eccab4
Josh Pollock <jpollock412@gmail.com>
2015-07-28 01:26:53 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+![Banner](https://calderawp.com/wp-content/uploads/2015/04/cwp-logo-horizontal.png)
+
+Adanced Custom Fields Magic Tag
+===============================
+
+Adds a magic tag for Advanced Custom Fields to [CalderaWP magic tags](https://github.com/CalderaWP/magic-tags).
+
+### License & Copyright
+* Copyright 2015 Josh Pollock for CalderaWP LLC.
+
+* Licensed under the terms of the [GNU General Public License version 2](http://www.gnu.org/licenses/gpl-2.0.html) or later.
+
+* Please share with your neighbor.
+
---
+++ src/acf_field.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Adds a magic tag for ACF's get_field
+ *
+ * @author    Josh Pollock <Josh@JoshPress.net>
+ * @license   GPL-2.0+
+ * @link
+ * @copyright 2015 Josh Pollock for CalderaWP LLC.
+ */
+
+namespace calderawp\filter\acf;
+
+
+class acf_field {
+
+	public function _construct() {
+		add_filter( 'caldera_magic_tag-acf_field', array( $this, 'do_magic' ) );
+	}
+
+	/**
+	 * Process with get_field()
+	 *
+	 * @uses "caldera_magic_tag-acf_field" filter
+	 *
+	 * @param array $params
+	 *
+	 * @return bool
+	 */
+	public function do_magic( $params ) {
+		if ( function_exists( 'get_field' ) ) {
+
+			$params = explode( ':', $params );
+
+			if( isset( $params[1] ) ){
+				$post = get_post( $params[0] );
+				$field = $params[1];
+
+			}else{
+				global $post;
+				$field = $params[0];
+
+			}
+
+			if ( is_object( $post ) && isset( $post->ID) ) {
+				$post_id =  $post->ID;
+			}else{
+				$post_id = false;
+			}
+
+			return get_field( $field, $post_id );
+		}
+
+	}
+}
